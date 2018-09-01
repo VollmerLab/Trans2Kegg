91878f713882022f88b648b67ea7dbc32c443a89
croesel <croesel@gmail.com>
2018-09-01 09:31:20 -0500

    Catch BLAST errors

--- R/Trans2Kegg.R
+++ R/Trans2Kegg.R
@@ -19,14 +19,14 @@ annotateTranscripts <- function(accessions, refTransFile){
   for (accession in accessions){
     transSeq <- refTrans[accession]
     seq <- as.character(transSeq)
-    blastResult <- blastSequences(paste0(">",accession,"\n", seq),
+    blastResult <- tryCatch(blastSequences(paste0(">",accession,"\n", seq),
       database="swissprot",
-      program="blastx",as="data.frame", expect=1e-5)
+      program="blastx",as="data.frame", expect=1e-5),
+      error=function(e) print(accession))
     if(length(blastResult) > 0){
       blastResult <- subset(blastResult, select=c("Iteration_query-def",
         "Iteration_query-len", "Hit_accession", "Hit_len", "Hsp_evalue",
         "Hsp_identity", "Hsp_gaps", "Hsp_align-len"))
-      print(blastResult)
       uniprots <- unique(blastResult$Hit_accession)
       for (uniprot in uniprots){
         uniKegg <- keggConv("genes", paste0("up:", uniprot))
@@ -38,13 +38,15 @@ annotateTranscripts <- function(accessions, refTransFile){
             ortho <- koDetail[[1]]$ORTHOLOGY
             if(length(names(ortho)) > 0){
               rowNum <- rowNum + 1
-              newOrtho <- data.frame(row.names=kegg, "kegg"=kegg, "ko"=names(ortho),
+              newOrtho <- data.frame(row.names=kegg,
+                "kegg"=kegg, "ko"=names(ortho),
                 "desc"=ortho[names(ortho)])
               uniToKO <- merge(newRow, newOrtho)
               #print(uniToKO)
               blastToKO <- merge(uniToKO,blastResult, by.x="uniprot",
                 by.y="Hit_accession")
-              write.table(blastToKO, file="blastToKO.csv", col.names=(rowNum ==1),
+              write.table(blastToKO, file="blastToKO.csv",
+                col.names=(rowNum ==1),
                 append=(rowNum != 1), row.names=FALSE)
             }
           }
