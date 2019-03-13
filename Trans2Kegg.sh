5cfea5f819c63aa581ea4a16d60871cffdd91ff9
roesel-c <roesel.c@husky.neu.edu>
2019-03-13 07:16:29 -0400

    Remove NCBI annotation to try and meet 5 min limit

--- Trans2Kegg.sh
+++ Trans2Kegg.sh
@@ -3,7 +3,7 @@ Rscript rox.R
 cd ..
 R CMD build Trans2Kegg \
     1>Trans2Kegg/build.log 2>Trans2Kegg/build.err
-R CMD check Trans2Kegg_0.99.25.tar.gz \
+R CMD check --timings Trans2Kegg_0.99.25.tar.gz \
     1>>Trans2Kegg/build.log 2>>Trans2Kegg/build.err
 R CMD BiocCheck Trans2Kegg_0.99.25.tar.gz \
     1>>Trans2Kegg/build.log 2>>Trans2Kegg/build.err
